17225e49960d7aab141657bee0e077ac99af8501
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-18 22:14:21 -0500

    chore: converts to new prompt v4 system etc

--- init.zsh
+++ init.zsh
@@ -38,7 +38,7 @@ p6df::modules::julia::init() {
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::jl::env::prompt::info()
+# Function: str str = p6df::modules::jl::prompt::env()
 #
 #  Returns:
 #	str - str
@@ -46,9 +46,41 @@ p6df::modules::julia::init() {
 #  Environment:	 JLENV_ROOT
 #>
 ######################################################################
-p6df::modules::jl::env::prompt::info() {
+p6df::modules::jl::prompt::env() {
 
   local str="julia_root:\t  $JLENV_ROOT"
 
   p6_return_str "$str"
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::jl::prompt::lang()
+#
+#>
+######################################################################
+p6df::modules::jl::prompt::lang() {
+
+  local ver
+
+  local ver_mgr
+  ver_mgr=$(jlenv version-name 2>/dev/null)
+  if p6_string_eq "$ver_mgr" "system"; then
+    local ver_sys="sys@"
+    local v
+    v=$(julia -v | awk '{print $3}')
+    if p6_string_blank "$v"; then
+      ver_sys="sys:no"
+    fi
+    ver="$ver_sys"
+  else
+    ver="$ver_mgr"
+  fi
+
+  local str="jl:$ver"
+
+  p6_return "$str"
+}
+
 }
