216711b70079952df34cbea8393b41a0dded0dcf
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 11:03:27 -0400

    fix(prompt): refactor julia prompt to use p6_return_words and langmgr::init pattern

--- init.zsh
+++ init.zsh
@@ -17,40 +17,16 @@ p6df::modules::julia::deps() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::julia::init(_module, dir)
-#
-#  Args:
-#	_module -
-#	dir -
+# Function: p6df::modules::julia::langmgr::init()
 #
 #  Environment:	 P6_DFZ_SRC_DIR
 #>
 ######################################################################
-p6df::modules::julia::init() {
-  local _module="$1"
-  local dir="$2"
+p6df::modules::julia::langmgr::init() {
 
   p6df::core::lang::mgr::init "$P6_DFZ_SRC_DIR/HiroakiMikami/jlenv" "jl"
 
   p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: str str = p6df::modules::jl::prompt::env()
-#
-#  Returns:
-#	str - str
-#
-#  Environment:	 JLENV_ROOT
-#>
-######################################################################
-p6df::modules::jl::prompt::env() {
-
-  local str="julia_root:\t  $JLENV_ROOT"
-
-  p6_return_str "$str"
 }
 
 ######################################################################
@@ -72,4 +48,20 @@ p6df::modules::jl::prompt::lang() {
     "julia -v | p6_filter_column_pluck 3")
 
   p6_return_str "$str"
+}
+
+######################################################################
+#<
+#
+# Function: words julia $JLENV_ROOT = p6df::modules::julia::prompt::env()
+#
+#  Returns:
+#	words - julia $JLENV_ROOT
+#
+#  Environment:	 JLENV_ROOT
+#>
+######################################################################
+p6df::modules::julia::prompt::env() {
+
+  p6_return_words 'julia' '$JLENV_ROOT'
 }
